4b76972ecffe7f992d9ca24e0577d901b91966dc
Venkatesh Pothagani <venkateshpothagani@gmail.com>
2022-04-10 17:11:52 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormEvent, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const intialMessage = Object.freeze('');
+	const [message, setMessage] = useState(intialMessage);
+
+	const onInputChange = (event: any) => {
+		setMessage(event.target.value);
+	};
+
+	const onFormSubmit = (event: FormEvent) => {
+		event.preventDefault();
+	};
+
+	return (
+		<div className="App">
+			<div className="chatBox">{message}</div>
+			<form method="post" onSubmit={onFormSubmit}>
+				<input type="text" onChange={onInputChange} />
+				<input type="submit" value="Send" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
